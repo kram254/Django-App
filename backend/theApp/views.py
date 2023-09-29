d459455137bfdb6047ae48b695463a8d401ef6f3
Emmanuel  M.  Ndaliro <33391934+kram254@users.noreply.github.com>
2023-09-29 16:29:08 +0300

    Updated1

--- backend/theApp/views.py
+++ backend/theApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
-    return HttpResponse(" Hello karios !!")
+    return render (request, "home.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    # return render (request,"todos.html",{"todo_list": ["Debug my code","Buy milk","Go to gym"]})
+    return render (request,"todos.html",{"todos": items})
